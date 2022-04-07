1fa6d4c558be537614907f138ea782954b6d5825
Jingrui Wei <jwei74@wisc.edu>
2022-04-07 16:55:21 -0500

    Add pixelsize loop, switch to low-RAM method

--- simulation/simulationScript.py
+++ simulation/simulationScript.py
@@ -1,9 +1,4 @@
 '''
-The input cif file and should be under the current working directory. 
-
-python testing.py tiffile miller_indices pixelsize_angstrom
-Example: python testing.py Silicon.cif 100 0.4
-
 Created: Shreya Jagadeshwaran
 Modified: Jingrui Wei
 '''
@@ -22,28 +17,18 @@ def structure_prep():
   file = read(os.getcwd() + '/' + sys.argv[1])
   file.center()
 
-  #Print contents of cif file for testing
-  f = open(sys.argv[1], 'r')
-  content = f.read()
-  print(content)
-
-
-  #Volume : 50,50,600 angstrom
-  #use 10,10 angstrom for test running
-  XY = 50/(file.cell.cellpar()[0])
+  #Max thickness: 60 nm
   Z = 600/(file.cell.cellpar()[2])
 
-  mindices = int(open(sys.argv[2], 'r').read())
-
-  print("Miller Indices: " + str(mindices))   #Print miller indices for testing
+  mindices = int(sys.argv[2])
   i1 = (int)(mindices%10)
   mindices/=10
   i2 = (int)(mindices%10)
   mindices/=10
   i3 = (int)(mindices%10)
-  stucture = surface(file, indices=(i1, i2, i3), layers=int(Z), periodic=True)
+  stucture = surface(file, indices=(i1, i2, i3), layers=int(Z), periodic=True) # tile along z direction
  
-  stucture*=(int(XY),int(XY),1)
+  #stucture*=(int(XY),int(XY),1)
 
   from abtem.structures import orthogonalize_cell 
   stucture_o = orthogonalize_cell(stucture)
@@ -51,7 +36,7 @@ def structure_prep():
   return stucture_o
 
 
-def simulate(struct_o): 
+def simulate(struct_o, pixelsize): 
   from abtem.temperature import FrozenPhonons  
   from abtem.measure import Measurement
 
@@ -59,12 +44,9 @@ def simulate(struct_o):
   #fp = FrozenPhonons(struct_o, num_configs = int(sys.argv[3]), sigmas = sigmadict)
 
   probe = Probe(energy=200e3, semiangle_cutoff=24.5, defocus=0, device='gpu') # calculate the wave function of electron probe
-  potential = Potential(struct_o, sampling = 0.01, slice_thickness=2, projection='infinite', parametrization='kirkland', device ='gpu')
+  potential = Potential(struct_o, sampling = 0.01, slice_thickness=0.4, projection='infinite', parametrization='kirkland', device ='gpu')
 
   from abtem.scan import GridScan
-  pixelsize =(float)(open(sys.argv[3], 'r').read()) # probe scan step size in angstrom, varies between 0.05, 0.15, 0.25, 0.35, 0.45 angstrom
-  
-  print("Pixel Size: " + str(pixelsize))  #Print pixel size for testing
 
   # integrate potential and generate image for thickness of 12,24,36,48,60 nm
   # Go thickness dowm to taking use of intermediate result in memory
@@ -73,16 +55,20 @@ def simulate(struct_o):
   #print(len(potential), slicelist)
   for slicenum in slicelist:
     potential_slice = potential[:slicenum]
-    print('Potential integrated for ', str(slicenum*0.4), ' nm') 
+    print('Potential integrated for ', str(slicenum*0.04), ' nm') 
     detector = AnnularDetector(inner=100, outer=350) # define the detector that collects eletrons and generate the final STEM image. 
-    # define the scan grid, use fixed grid size 100 for uniform image size    
-    gridscan = GridScan(start = [0,0], end = [pixelsize*100,pixelsize*100], gpts=(100,100), sampling=pixelsize) # define the scan grid
+    # define the scan grid    
+    gridscan = GridScan(start = [0,0], end = [struct_o.cell.cellpar()[0],struct_o.cell.cellpar()[1]], sampling=pixelsize) # define the scan grid
 
     measurement_files = probe.scan(gridscan, [detector], potential_slice, pbar=True) # run simulation
-    images.append(measurement_files.array)
+    X = 100//measurement_files.array.shape[0] + 1
+    Y = 100//measurement_files.array.shape[1] + 1
+    new_measurement = measurement_files.tile((X, Y))
+    images.append(new_measurement.array[:100,:100])
   
   #gets atomic x,y coordinates of orthogonalized cell in angstrom
-  posxy = np.unique(np.round(struct_o.get_positions()[:,:2],4), axis = 0)
+  struct_tile = struct_o*(X,Y,1)
+  posxy = np.unique(np.round(struct_tile.get_positions()[:,:2],4), axis = 0)
   cropped = filter(lambda point: (point[0] < pixelsize*100) & (point[1] < pixelsize*100), posxy)
   cropped = np.array(list(cropped))
   # save the simulated images and coordinates in a single npz file
@@ -97,4 +83,9 @@ def simulate(struct_o):
 
 
 if __name__ == "__main__":
-    simulate(structure_prep())
+    # probe scan step size in angstrom varies between 0.05, 0.15, 0.25, 0.35, 0.45
+    simulate(structure_prep(), pixelsize=0.45)
+    simulate(structure_prep(), pixelsize=0.35)
+    simulate(structure_prep(), pixelsize=0.25)  
+    simulate(structure_prep(), pixelsize=0.15)
+    simulate(structure_prep(), pixelsize=0.05)  
