f3fcf7835dd1bc83c84da7be5a11f2758f86967b
Naveen Balu <nbalu@wisc.edu>
2022-04-27 16:04:51 -0500

    Replaced test script with real training script

--- model/modelTestScript.py
+++
@@ -1,6 +0,0 @@
-#Test python file - Replace this file with the real python model training script!
-# f = open('/data/testCIF', 'r')
-# content = f.read()
-# print(content)
-# f.close()
-print("Im a model test script!")
