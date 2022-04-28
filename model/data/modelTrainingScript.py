60376ba33f164dee495843ad9bf2f967258cdfd3
Naveen Balu <nbalu@wisc.edu>
2022-04-28 16:45:24 -0500

    Update training script

--- model/data/modelTrainingScript.py
+++ model/data/modelTrainingScript.py
@@ -15,19 +15,16 @@ def makeModel():
   images_test = data['X_test']
   labels_test = data['y_test']
   print('Data loaded.')
-  
+ 
   # Use nb_classes=1 for our training data. 
   # SegResNet is designed for such segmentation task. 
   # You can also try other availble architecture like 'Unet' or custom ones.
-  model_semantic = aoi.models.Segmentor(model = 'SegResNet', nb_classes=3)
+  model_semantic = aoi.models.Segmentor(model = 'SegResNet', nb_classes=1)
 
   print('Start training.')
-  print('Test message')
-  
   # You can use the much smaller training_cycles for testing, adjust it according to training convergence
   model_semantic.fit(images_all, labels_all, images_test, labels_test, training_cycles=500, 
   plot_training_history = False, compute_accuracy = True, swa=True, filename = 'model_'+ curDT.strftime("%m%d_%H%M"))
-  print('Successfully trained model')
- 
+  
 if __name__ == "__main__":
     makeModel()
