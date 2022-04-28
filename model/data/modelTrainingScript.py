6ca484f47405991f1f565cc6b9feb04596cb4aa8
Naveen Balu <nbalu@wisc.edu>
2022-04-28 15:14:43 -0500

    Added training script to data to be volume mounted

--- model/data/modelTrainingScript.py
+++ model/data/modelTrainingScript.py
@@ -15,13 +15,14 @@ def makeModel():
   images_test = data['X_test']
   labels_test = data['y_test']
   print('Data loaded.')
- 
+  
   # Use nb_classes=1 for our training data. 
   # SegResNet is designed for such segmentation task. 
   # You can also try other availble architecture like 'Unet' or custom ones.
   model_semantic = aoi.models.Segmentor(model = 'SegResNet', nb_classes=3)
 
   print('Start training.')
+  
   # You can use the much smaller training_cycles for testing, adjust it according to training convergence
   model_semantic.fit(images_all, labels_all, images_test, labels_test, training_cycles=500, 
   plot_training_history = False, compute_accuracy = True, swa=True, filename = 'model_'+ curDT.strftime("%m%d_%H%M"))
