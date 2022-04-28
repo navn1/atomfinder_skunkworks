1ebeb064fac342e1a8b35d5d729302c5b171718f
Naveen Balu <nbalu@wisc.edu>
2022-04-28 17:17:09 -0500

    Fixed model output destination

--- model/data/modelTrainingScript.py
+++ model/data/modelTrainingScript.py
@@ -24,7 +24,7 @@ def makeModel():
   print('Start training.')
   # You can use the much smaller training_cycles for testing, adjust it according to training convergence
   model_semantic.fit(images_all, labels_all, images_test, labels_test, training_cycles=500, 
-  plot_training_history = False, compute_accuracy = True, swa=True, filename = 'model_'+ curDT.strftime("%m%d_%H%M"))
+  plot_training_history = False, compute_accuracy = True, swa=True, filename = './data/model_'+ curDT.strftime("%m%d_%H%M"))
   
 if __name__ == "__main__":
     makeModel()
