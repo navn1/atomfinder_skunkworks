61d0ab44026557b3ea08ede4e8387e9403161847
Jingrui Wei <jwei74@wisc.edu>
2022-06-06 10:44:41 -0500

    load data once for loop

--- model/data/modelTrainingScript.py
+++ model/data/modelTrainingScript.py
@@ -7,14 +7,9 @@ import numpy as np
 import os, sys
 from datetime import datetime
 
-def makeModel(learningRate):
-  curDT = datetime.now()
-  data = np.load(os.getcwd() + '/' + sys.argv[1], allow_pickle=True)
-  images_all = data['X_train']
-  labels_all = data['y_train']
-  images_test = data['X_test']
-  labels_test = data['y_test']
-  print('Data loaded.')
+def makeModel(learningRate, X_train, y_train, X_test, y_test):
+  #curDT = datetime.now()
+
  
   # Use nb_classes=1 for our training data. 
   # SegResNet is designed for such segmentation task. 
@@ -24,9 +19,15 @@ def makeModel(learningRate):
   print('Start training.')
   # You can use the much smaller training_cycles for testing, adjust it according to training convergence
   # Test changing loss function to mse instead of default ce
-  model_semantic.fit(images_all, labels_all, images_test, labels_test, lr_scheduler=[learningRate], training_cycles=500, 
+  model_semantic.fit(X_train, y_train, X_test, y_test, lr_scheduler=[learningRate], training_cycles=500, 
   plot_training_history = True, compute_accuracy = True, swa=True, filename = './data/model_'+ sys.argv[1][5:-4] + 'lr'+learningRate)
   
 if __name__ == "__main__":
+  data = np.load(os.getcwd() + '/' + sys.argv[1], allow_pickle=True)
+  images_all = data['X_train']
+  labels_all = data['y_train']
+  images_test = data['X_test']
+  labels_test = data['y_test']
+  print('Data loaded.')
   for lr in [10^-6,(10^-6+10^-5)/2,10^-5,(10^-5+10^-4)/2,1*10^-4,(10^-4+10^-3)/2,1*10^-3]:
-    makeModel(lr)
+    makeModel(lr, images_all, labels_all, images_test, labels_test)
